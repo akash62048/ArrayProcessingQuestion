c9581140707de59bdc8230709fb5b05b3118acbf
Akash <akashpoddar492@gmail.com>
2025-08-29 16:08:52 +0530

    Problem number 1 solved--- Total number of elements = 1000.

---
+++ ArraySolutions/src/TotalNumberOfElements.java
@@ -0,0 +1,7 @@
+public class TotalNumberOfElements {
+    public static void main(String[] args) {
+
+        int[] numbers = GivenArray.givenArray;
+        System.out.println("Total number of elements = " + (numbers.length));
+        }
+    }
